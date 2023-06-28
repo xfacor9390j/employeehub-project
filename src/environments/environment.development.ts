9a4acf6949189bee4c45c3d4d08072c2a071428e
Xfactor9390 <78318055+Xfactor9390@users.noreply.github.com>
2023-06-28 22:06:56 +0530

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment: { production:boolean,apiBaseUrl:string} = {
+    production: false,
+    apiBaseUrl:'http://localhost:8080'
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment: { production:boolean,apiBaseUrl:string} = {
+    production: false,
+    apiBaseUrl:'http://localhost:8080'
+};
